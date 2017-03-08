8185ec7878db121bd7bb3cd63cb7dc7d7a8a3bfe
Michael <michael.melendezm@gmail.com>
2017-03-08 17:35:36 -0600

    Merge origin master

--- Business/ImageBusiness.php
+++ Business/ImageBusiness.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../Data/ImageData.php';
+include './Data/ImageData.php';
 
 class ImageBusiness {
 
@@ -10,8 +10,21 @@ class ImageBusiness {
         $this->imageData = new ImageData();
     }
 
+    public function insertTBImage($image) {
+        return $this->imageData->insertTBImage($image);
+    }
+
+    public function updateTBImage($image) {
+        return $this->imageData->updateTBImage($image);
+    }
+
+    public function deleteTBImage($idImage) {
+        return $this->imageData->deleteTBImage($idImage);
+    }
+
     public function getAllTBImages() {
         return $this->imageData->getAllTBImages();
     }
     
 }
+
